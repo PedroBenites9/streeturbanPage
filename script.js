2fa5bea69b2c1650c4ce841154251d8cfec008ee
Pedro Benites <pedro.benites99@hotmail.com>
2024-12-20 01:05:11 -0300

    Adding functionality to the page

--- script.js
+++ script.js
@@ -1,35 +1,96 @@
+const carritoArticulos = []
 
-// const produ = () => {
-//     fetch('https://fakestoreapi.com/products')
-//         .then(res => res.json())
-//         .then((json) => { return json })
-//         .catch(error => {
-//             console.log(error)
-//         })
+const carrito = document.querySelector("#carrito")
+const contenedorCarrito = document.querySelector('#lista-carrito')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
 
-// }
+const listaProducto = document.querySelector('#lista-producto')
+const contenedorCard = document.querySelector('.cards')
+
+const API_URL = './assets/json/Productos_Indumentaria.json'
 
 
-const API_URL = ' https://api.escuelajs.co/api/v1/products'
-const contenedorCard = document.querySelector('#card')
+cargarEventListeners()
+function cargarEventListeners() {
+    listaProducto.addEventListener('click', agregarCarrito)
+}
+
 fetch(API_URL)
     .then(response => response.json())
     .then(json => {
         json.forEach(clothes => {
             let div_card = document.createElement('div')
-            let title_card = document.createElement('h3')
+            const { images, title, price, id } = clothes
             div_card.classList.add('card')
             div_card.innerHTML = ` 
-            <img src=${clothes.images[1]} alt="imagenCard" referrerpolicy="no-referrer"/>
-            <p class="card__title">${clothes.title}</p>
-            <button class="button-buy">comprar</button>`
+            <div class='col'>
+                <img src=${images[1]} class="card-img-top p-0 w-100" alt="imagenCard" referrerpolicy="no-referrer"/>
+                <div class='card-body position-relative row row-cols-1  h-100 '>
+                    <div class='col'>
+                        <h4 class="card__title">${title}</h4>
+                        <p>$${price}</p>
+                        <a class="btn btn-light align-self-end agregar-carrito" data-id="${id}"><span> Añadir carrito</span></a>
+                    </div
+                </div>
+            </div>
+            `
             contenedorCard.append(div_card)
-
-            if (clothes.title == 'Product from the website') {
-                xhr.open("DELETE", `https://api.escuelajs.co/api/v1/products/${clothes.id}`);
-                xhr.send()
-            }
         })
     })
 
-const xhr = new XMLHttpRequest();
+
+// funciones
+function obtenerProducto(e) {
+    console.log('click desde obtener producto')
+}
+
+function agregarCarrito(e) {
+    e.preventDefault()
+
+    const contenidoCarrito = e.target.classList.contains('agregar-carrito')
+    const productoSeleccionado = e.target.parentElement.parentElement
+
+    if (productoSeleccionado) {
+        leerDatosProducto(contenidoCarrito)
+    }
+
+}
+
+function leerDatosProducto(curso) {
+    const infoProducto = {
+        idCurso: curso.querySelector('a').getAttribute('data-id'),
+        imagen: curso.querySelector('img').src,
+        title: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('span').textContent,
+        cantidad: 1
+    }
+}
+
+function carritoHTML() {
+    // limpiar HTML
+    limpiarHTML()
+    // recorre el carrito y genera HTML
+    carritoArticulos.forEach((curso) => {
+        const { imagen, title, precio, cantidad, idCurso } = curso
+        const row = document.createElement('tr')
+        row.innerHTML = `
+         <td>
+             <img src="${imagen}"    
+         </td>
+         <td>
+             ${title} 
+         </td>
+         <td>
+             ${precio}
+         </td>
+         <td>
+             ${cantidad}
+         </td>
+         <td>
+             <a href='#' class='borrar-curso' data-id="${idCurso}"> X </a>
+         </td>
+         `
+        // agregar HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row)
+    })
+}
